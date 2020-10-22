9c77ad610cfa9013a53d8359f10ff3285219640f
keisuketomita <keisuke.tommy@gmail.com>
2020-10-22 03:16:19 +0000

    投稿編集・削除

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit, :update, :destroy]
   def index
     @blogs = Blog.all
   end
@@ -24,13 +25,21 @@ class BlogsController < ApplicationController
   def edit
   end
   def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました"
+    else
+      render :edit
+    end
   end
   def destroy
+    @blog.destroy
+    redirect_to blogs_path
   end
   private
   def blog_params
     params.require(:blog).permit(:content, :image, :image_cache)
   end
   def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
