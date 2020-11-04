2e07e9c578be5b77c73e143eb4796ffe673e95e5
富田圭輔 <tomitakeisuke@tomitakeisukenoMacBook-Pro.local>
2020-11-04 20:34:40 +0900

    認証状態ごとのページアクセス権限設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  before_action :logged_in?, only: [:show]
+  before_action :authenticate_user, only: [:show]
   def new
     @user = User.new
   end
@@ -16,5 +19,8 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
