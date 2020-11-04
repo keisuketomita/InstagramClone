c764f2ca6ffa23fbcc3503732e9c745c5fe60cad
富田圭輔 <tomitakeisuke@tomitakeisukenoMacBook-Pro.local>
2020-11-04 22:01:11 +0900

    ユーザー情報編集、ユーザー画像登録

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
-  before_action :logged_in?, only: [:show]
-  before_action :authenticate_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in?, only: [:show, :edit]
+  before_action :authenticate_user, only: [:show, :edit]
   def new
     @user = User.new
   end
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_params_new)
     if @user.save
       redirect_to user_path(@user.id)
     else
@@ -16,9 +16,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def edit
+  end
+  def update
+    if @user.update(user_params_edit)
+      redirect_to user_path(@user.id), notice: "ユーザーを編集しました"
+    else
+      render :edit
+    end
+  end
   private
-  def user_params
+  def user_params_new
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  def user_params_edit
+    params.require(:user).permit(:name, :email, :image)
   end
   def set_user
     @user = User.find(params[:id])
--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,5 @@ module Instagram
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
   end
 end
