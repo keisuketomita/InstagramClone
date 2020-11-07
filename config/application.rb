2d8a0013578af2447c16cecef9aac715f5531fa4
富田圭輔 <tomitakeisuke@tomitakeisukenoMacBook-Pro.local>
2020-11-07 17:27:18 +0900

    ヘッダー、投稿一覧、ユーザーのページデザイン

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,11 @@ module Instagram
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.load_defaults 5.1
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
